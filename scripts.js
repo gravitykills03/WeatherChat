310782fadca3805d59ef37ea85798abd1dcd77d3
craigkelleher <kelleher.craig@gmail.com>
2022-11-02 08:51:52 -0700

    Deleted Duplicate Weather Code Fixed accidental deletion of previous weather fix Refactored code and standardized variable naming

--- scripts.js
+++ scripts.js
@@ -18,39 +18,34 @@ const popover2 = new bootstrap.Popover(document.getElementById("emoji_icon_box")
 })
 
 // weather widget script
-// fa5d040af9254b09862fe801f0e26f5f
-// 18a14438c1564f54830311ea69473030
-const apiKey = "18a14438c1564f54830311ea69473030";
+// API_KEY = "fa5d040af9254b09862fe801f0e26f5f";
+// API_KEY = "18a14438c1564f54830311ea69473030";
+
+//API KEY to access weatherbit API
+const API_KEY = "18a14438c1564f54830311ea69473030";
 document
   .querySelector("#search_input")
   .addEventListener("keydown", function (e) {
     let userInput = document.querySelector("#search_input").value;
-    const weatherApiUrl = `https://api.weatherbit.io/v2.0/current?&city=${userInput}&units=I&key=${apiKey}`;
+    const API_URL = `https://api.weatherbit.io/v2.0/current?&city=${userInput}&units=I&key=${API_KEY}`;
     if (e.key === "Enter") {
-      fetch(weatherApiUrl)
+      fetch(API_URL)
         .then((response) => response.json())
         .then((data) => {
-          // grab the weather image, temp, and description from API
-          const location_input = data.data[0]["city_name"];
-          const temp = data.data[0].temp;
-          const icon = data.data[0].weather.icon;
-          const desc = data.data[0].weather.description;
-          document.getElementById("location").innerText = location_input;
+          
+          // select the weather image, temp, and description elements and update values from API
+          document.getElementById("location").innerText = data.data[0]["city_name"];
           document.querySelector(
             ".icon"
-          ).src = `https://www.weatherbit.io/static/img/icons/${icon}.png`;
-
-          //Select the elements and apply css properties and update value
-          const tempDivChild = document.getElementById("temp_info");
-          tempDivChild.innerText = `${temp}°F`;
-          tempDivChild.classList.add("weather_text_style");
-          tempDivChild.style.cssText = "visibility : visible";
+          ).src = `https://www.weatherbit.io/static/img/icons/${data.data[0].weather.icon}.png`;
+          document.getElementById("temperature_info").innerText = `${data.data[0].temp}°F`;
+          document.getElementById("weather_description").innerText = data.data[0].weather.description;
 
           // reset text field to blank
           document.querySelector("#search_input").value = "";
         });
     }
-  });
+});
 
 //send messege through ws
 const ws = new WebSocket("ws://localhost:8080");
